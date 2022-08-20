89d59edd9932a7da32cd5244584a13560e1d9bef
najmulmyself <najmul.myself@gmail.com>
2022-08-20 17:23:37 +0600

    added btn with signin

--- lib/screens/sign_in.dart
+++ lib/screens/sign_in.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
@@ -17,7 +19,88 @@ class _SignInState extends State<SignIn> {
           width: double.infinity,
           color: Color(0xff0ABAB5),
         ),
-        Text('Sign In'),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 18.0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Sign In',
+                style: TextStyle(fontSize: 25),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(0xff0ABAB5),
+                      width: 2,
+                    ),
+                  ),
+                  labelText: 'Email',
+                  labelStyle: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  suffixIcon: GestureDetector(
+                      onTap: () {
+                        print('pressed');
+                        setState(() {
+                          Icon(
+                            Icons.remove_red_eye,
+                            color: Colors.blue,
+                          );
+                        });
+                        // }, child: Icon(Icons.visibility_off)),
+                      },
+                      child: Icon(
+                        Icons.remove_red_eye_rounded,
+                        color: Colors.grey,
+                      )),
+                  labelText: 'Password',
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(0xff0ABAB5),
+                      width: 2,
+                    ),
+                  ),
+                  labelStyle: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 80,
+              ),
+              Container(
+                height: 50,
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(
+                      Color(0xff0ABAB5),
+                    ),
+                  ),
+                  onPressed: () {
+                    print('pressed');
+                  },
+                  child: Text(
+                    'Sign In',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        )
       ],
     );
   }
