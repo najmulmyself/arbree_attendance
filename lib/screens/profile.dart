529fe7d01a84751ccfeb3082b5d9c3c1016aa8a4
najmulmyself <najmul.myself@gmail.com>
2022-08-23 12:39:07 +0600

    need to show data

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_unnecessary_containers
+
 import 'dart:convert';
 
 import 'package:arbree_attendance/model/user.dart';
@@ -5,42 +7,56 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
-class Profile extends StatelessWidget {
+class Profile extends StatefulWidget {
   const Profile({Key? key}) : super(key: key);
 
   @override
+  State<Profile> createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  var user;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getToken();
+  }
+
+  String?
+      token; // inital value will be empty string if it is null then occurs a problem accessing token
+  final url =
+      'https://personal-attendance.herokuapp.com/api/v1.0/accounts/user/profile/';
+  getUser(token) {
+    print('Hello : $token');
+    http.get(
+      Uri.parse(url),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token',
+      },
+    ).then((res) {
+      final response = jsonDecode(res.body);
+      print(res.body);
+      final user = Data.fromJson(response['data']);
+      print(user.email);
+    });
+  }
+
+  getToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('token');
+    print(' token is from SharedP getToken $token');
+    getUser(token);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    String?
-        token; // inital value will be empty string if it is null then occurs a problem accessing token
-    final url =
-        'https://personal-attendance.herokuapp.com/api/v1.0/accounts/user/profile/';
-    getUser(token) {
-      print('Hello : $token');
-      http.get(
-        Uri.parse(url),
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer $token',
-        },
-      ).then((res) {
-        final response = jsonDecode(res.body);
-        print(res.body);
-        final data = Data.fromJson(response['data']);
-        print(data.email);
-      });
-    }
-
-    getToken() async {
-      final prefs = await SharedPreferences.getInstance();
-      final token = prefs.getString('token');
-      print(' token is from SharedP getToken $token');
-      getUser(token);
-    }
-
     return Scaffold(
       body: Container(
         child: Center(
-          child: GestureDetector(onTap: getToken, child: Text('Profile')),
+          child: Text('${user}'),
         ),
       ),
     );
