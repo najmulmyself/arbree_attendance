bd315aa826b18fedeb9b082e0fe97698c8797888
najmulmyself <najmul.myself@gmail.com>
2022-09-24 15:22:54 +0600

    added circular indi

---
+++ lib/component/rounded_circle.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class RoundedCircle extends StatelessWidget {
+  RoundedCircle({
+    required this.title,
+    this.color,
+    required this.number,
+    this.border,
+  });
+  final String title;
+  final Color? color;
+  final int number;
+  final Color? border;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: CircleAvatar(
+        radius: 31,
+        backgroundColor: border,
+        child: CircleAvatar(
+          radius: 29,
+          backgroundColor: color ?? Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  number.toString(),
+                  style: TextStyle(
+                    fontSize: 19,
+                    fontWeight: FontWeight.bold,
+                    color: border,
+                  ),
+                ),
+                Text(
+                  title,
+                  style: TextStyle(fontSize: 12, color: border),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/reports_page.dart
+++ lib/screens/reports_page.dart
@@ -1,9 +1,22 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
+import 'package:arbree_attendance/component/rounded_circle.dart';
 import 'package:flutter/material.dart';
 
-class ReportScreen extends StatelessWidget {
+class ReportScreen extends StatefulWidget {
   const ReportScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ReportScreen> createState() => _ReportScreenState();
+}
+
+class _ReportScreenState extends State<ReportScreen> {
+  List items = [
+    'This Months',
+    'September 2022',
+    'August 2022',
+    'July 2022',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -37,8 +50,80 @@ class ReportScreen extends StatelessWidget {
         ],
         centerTitle: true,
       ),
-      body: Center(
-        child: Text('Reports'),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Column(
+            children: [
+              SizedBox(
+                height: 30,
+              ),
+              DropdownButton(
+                // value: items[0],
+                icon: Icon(
+                  Icons.calendar_today,
+                ),
+                hint: Text('This Months'),
+                underline: Container(
+                  height: 2,
+                  color: Colors.grey,
+                ),
+                items: items
+                    .map(
+                      (shift) => DropdownMenuItem(
+                        child: Text(shift),
+                        value: shift,
+                      ),
+                    )
+                    .toList(),
+                onChanged: (value) {},
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  RoundedCircle(
+                    border: Color(0xff374552),
+                    title: 'Present',
+                    number: 22,
+                  ),
+                  // SizedBox(
+                  //   width: 10,
+                  // ),
+                  RoundedCircle(
+                    color: Color(0xffFAD2D2),
+                    border: Color(0xffF0564A),
+                    title: 'ontime',
+                    number: 22,
+                  ),
+                  RoundedCircle(
+                    color: Color(0xffF8F8C9),
+                    border: Color(0xffFFB636),
+                    title: 'late',
+                    number: 22,
+                  ),
+                  RoundedCircle(
+                    color: Color(0xffD9F9F2),
+                    border: Color(0xff1AB394),
+                    title: 'absent',
+                    number: 22,
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Container(
+                    height: 200,
+                    width: double.infinity,
+                    color: Colors.red,
+                    child: Text('Chart'),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
