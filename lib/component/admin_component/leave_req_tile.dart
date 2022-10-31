a637b03add9d21c017c27ae1bbb89b9526d47f71
najmulmyself <najmul.myself@gmail.com>
2022-10-31 14:21:46 +0600

    added leave tile

---
+++ lib/component/admin_component/leave_req_tile.dart
@@ -0,0 +1,190 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+import '../../utils/utils.dart';
+
+class LeaveReqTile extends StatelessWidget {
+  const LeaveReqTile({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey, width: 2),
+          borderRadius: BorderRadius.circular(5),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: 20,
+              // width: double,
+              color: Colors.grey.shade300,
+              child: Center(
+                child: Text(
+                  'Available Leave : 5 days',
+                  style: TextStyle(
+                    color: Utils.colorBlue,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              child: Column(
+                children: [
+                  Text(
+                    "Paul Smith",
+                    style: TextStyle(
+                      color: Utils.colorPrimary,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  CircleAvatar(
+                    radius: 27,
+                    backgroundImage:
+                        NetworkImage("https://picsum.photos/200/300"),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.date_range_rounded,
+                        color: Utils.colorPrimary,
+                      ),
+                      Text(
+                        '10 sep 2022',
+                        style: TextStyle(
+                          color: Utils.colorBlue,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.badge,
+                            color: Utils.colorPrimary,
+                          ),
+                          Text(
+                            '1 Day',
+                            style: TextStyle(
+                              color: Utils.colorBlue,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.toys_rounded,
+                            color: Utils.colorPrimary,
+                          ),
+                          Text(
+                            'Vacation',
+                            style: TextStyle(
+                              color: Utils.colorBlue,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 40,
+              color: Color(0xff374552),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.remove_red_eye),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        "View",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.check,
+                        color: Utils.colorPrimary,
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, '/admin/newNotice');
+                        },
+                        child: Text(
+                          "Approve",
+                          style: TextStyle(
+                            color: Utils.colorPrimary,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.delete, color: Colors.red),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        "Reject",
+                        style: TextStyle(
+                          color: Colors.red,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/admin_screen/admin_leave_managemet.dart
+++ lib/screens/admin_screen/admin_leave_managemet.dart
@@ -1,9 +1,11 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:arbree_attendance/component/icon_rounded_circle.dart';
 import 'package:arbree_attendance/component/rounded_circle.dart';
 import 'package:arbree_attendance/utils/utils.dart';
 import 'package:flutter/material.dart';
+
+import '../../component/admin_component/leave_req_tile.dart';
 
 class AdminLeaveManagement extends StatelessWidget {
   const AdminLeaveManagement({Key? key}) : super(key: key);
@@ -18,38 +20,43 @@ class AdminLeaveManagement extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(vertical: 18.0),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                IconRoundCircle(
-                  color: Utils.colorRedLight,
-                  border: Utils.colorRed,
-                  hasIcon: false,
-                  title: 'Leave',
-                  title2: "Request",
-                ),
-                IconRoundCircle(
-                  color: Colors.white,
-                  border: Utils.colorBlue,
-                  hasIcon: false,
-                  title: 'Leave',
-                  title2: "Overview",
-                ),
-                IconRoundCircle(
-                  color: Utils.colorPrimaryLight,
-                  border: Utils.colorPrimary,
-                  hasIcon: false,
-                  title: 'Leave',
-                  title2: "Report",
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 20,
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  IconRoundCircle(
+                    color: Utils.colorRedLight,
+                    border: Utils.colorRed,
+                    hasIcon: false,
+                    title: 'Leave',
+                    title2: "Request",
+                  ),
+                  IconRoundCircle(
+                    color: Colors.white,
+                    border: Utils.colorBlue,
+                    hasIcon: false,
+                    title: 'Leave',
+                    title2: "Overview",
+                  ),
+                  IconRoundCircle(
+                    color: Utils.colorPrimaryLight,
+                    border: Utils.colorPrimary,
+                    hasIcon: false,
+                    title: 'Leave',
+                    title2: "Report",
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              LeaveReqTile(),
+              LeaveReqTile(),
+              LeaveReqTile(),
+            ],
+          ),
         ),
       ),
     );
